127998ef0168ecf8648b8675d1c1715193b907c1
yourboy7 <243864016@qq.com>
2025-06-09 00:15:35 +0800

    init

---
+++ src/DeployiOSSample/DeployiOSSample/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DeployiOSSample
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
